afeffab9d84cc8c905f10a5e0f50104d00046e3f
‘smilecamo99’ <yangyongju@shketai.com>
2022-05-26 14:17:35 +0800

    feat: 打包
    部署

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -4,7 +4,7 @@ const TIME_OUT = 10000;
 if (process.env.NODE_ENV === 'development') {
   BASE_URL = '/api';
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'http://coderwhy.org/prod';
+  BASE_URL = 'http://httpbin.org/prod';
 } else if (process.env.NODE_ENV === 'test') {
   BASE_URL = '';
 }
